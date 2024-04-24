2407f81698bb404217077db0c95a1320b931c913
Mohammad-Amin-Jenadele <aminmobin1920@gmail.com>
2024-04-24 14:08:52 +0330

    confusion matrix and load_cifar10 added

--- utils.py
+++ utils.py
@@ -25,3 +25,41 @@ def recall(y_true, y_pred):
     possible_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true, 0, 1)))
     recall = true_positives / (possible_positives + tf.keras.backend.epsilon())
     return recall
+
+# Plotting confusion matrix
+def plot_confusion_matrix(model , x , y ):
+
+    y_pred = model.predict(x) 
+    y_pred_plot=np.argmax(y_pred, axis=1)
+    y_test_plot=np.argmax(y, axis=1)
+
+    cm = confusion_matrix(y_test_plot, y_pred_plot)
+    cmn = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    fig, ax = plt.subplots(figsize=(10,10))
+    sns.heatmap(cmn, annot=True, fmt='.2f')
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.show(block=False)
+    
+
+# Defining load_cifar10 function that gets val_ratio as input and returns (x_train, y_train), (x_test, y_test), (x_val, y_val)
+def load_cifar10(val_ratio):
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    # Normalize pixel values to be between 0 and 1
+    x_train = x_train.astype('float32') / 255.0
+    x_test = x_test.astype('float32') / 255.0
+
+    # One hotting the labels
+    num_classes = 10
+    y_train = to_categorical(y_train, num_classes)
+    y_test = to_categorical(y_test, num_classes)
+
+    # Split training set into training and validation sets
+    split_index = int(len(x_train) * (1 - val_ratio))
+    x_val = x_train[split_index:]
+    y_val = y_train[split_index:]
+    x_train = x_train[:split_index]
+    y_train = y_train[:split_index]
+
+    return (x_train, y_train), (x_test, y_test), (x_val, y_val)
