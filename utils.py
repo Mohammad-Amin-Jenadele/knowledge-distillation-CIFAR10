1f37441464e69ce5e1e64258e812998cb407c84f
Mohammad-Amin-Jenadele <aminmobin1920@gmail.com>
2024-04-25 17:34:35 +0330

    Fixed MobileNetV2_model

--- utils.py
+++ utils.py
@@ -78,10 +78,11 @@ def sample_plot(x , y , num_to_class_dic):
 
 
 # Defining MobileNetV2_model that gives MobileNetV2 model
-def MobileNetV2_model(weights):
+def MobileNetV2_model(weights , input_shape):
     model = tf.keras.applications.MobileNetV2(
     include_top=False,
     weights = weights
+    input_shape = input_shape
     )
     return model
 
